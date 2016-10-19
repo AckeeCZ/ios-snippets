e5b253c167cc7a1a6ef9b1ce6734973ec797ad86
Tomas Kohout <email@tomaskohout.cz>
2016-10-19 13:06:03 +0200

    Fix

--- sources/ack_addtableview.swift
+++ sources/ack_addtableview.swift
@@ -3,8 +3,6 @@ let tableView = UITableView()
 tableView.delegate = self
 tableView.dataSource = self
 tableView.rowHeight =  UITableViewAutomaticDimension
-tableView.separatorColor = .clearColor()
-tableView.tableFooterView = UIView(frame: .zero)
 view.addSubview(tableView)
 tableView.snp_makeConstraints { make in
 make.edges.equalTo(0)
